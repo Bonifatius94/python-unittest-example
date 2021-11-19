5203e7baed84fd0a9eab18006f20d464ed397a1b
Marco Tröster <marco@troester-gmbh.de>
2021-11-19 18:04:46 +0100

    added test for second function

--- src/tests/test_my_module/test_myfunc.py
+++ src/tests/test_my_module/test_myfunc.py
@@ -1,5 +1,10 @@
 from my_module import MyFuncClass
 
-def test_should_be_1():
+def test_should_be_1_by_default():
     obj = MyFuncClass()
     assert(obj.get_attribute() == 1)
+
+def test_should_be_n_after_setting_attribute_to_n():
+    obj = MyFuncClass()
+    obj.set_attribute(2)
+    assert(obj.get_attribute() == 2)
